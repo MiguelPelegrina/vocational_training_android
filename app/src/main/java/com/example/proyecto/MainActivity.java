5b993d43beb422930e4b2394a4e587b406cda478
migup <migupele@gmail.com>
2022-11-18 19:06:01 +0100

    Empezando

---
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //Declaracion de variables
+    private Button btnLogin;
+    private Button btnRegistro;
+    private EditText txtUsuario;
+    private EditText txtContrasena;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Inicializacion de variables
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistro = (Button) findViewById(R.id.btnRegistro);
+        txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        txtContrasena = (EditText) findViewById(R.id.txtContrasena);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
