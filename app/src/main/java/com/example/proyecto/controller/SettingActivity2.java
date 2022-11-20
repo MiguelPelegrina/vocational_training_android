49b7427c8af8da4ec608df9ddd9c362e2b561f75
migup <migupele@gmail.com>
2022-11-20 11:13:45 +0100

    Login y registro funcionan por ahora Preparar segunda actividad después del login: menu que lleva a las preferencias, faltaría la parte de cargar las preferencias

---
+++ app/src/main/java/com/example/proyecto/controller/SettingActivity2.java
@@ -0,0 +1,45 @@
+package com.example.proyecto.controller;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.proyecto.R;
+import com.example.proyecto.fragments.SettingsFragment;
+
+public class SettingActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting2);
+        // Introducimos el fragment creado en el contenedor de la vista padre (activity_setting.xml)
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.setting_container, new SettingsFragment())
+                .commit();
+
+        // Activamos el icono de "Volver"
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    // Asociamos un oyente al evento de pulsar el icono de volver
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
